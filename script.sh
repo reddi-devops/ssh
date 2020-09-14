fb36b668f2104a78a3cebf94a006868498ee5258
root <root@instance-2.us-central1-a.c.kishore-275306.internal>
2020-09-14 22:01:31 +0530

    tt

--- script.sh
+++ script.sh
@@ -17,17 +17,11 @@ cp config /etc/selinux/
 service sshd restart
 #rmmod -v pcspkr
 echo "Installing ssmtp"
-sleep 5
 yum install ssmtp-2.61-11.5.3.x86_64.rpm -y
-sleep 5
 mv /sbin/sendmail /temp
-sleep 5
 ln -s /usr/sbin/ssmtp /usr/sbin/sendmail
-sleep 5
 cp -rp _smtp /etc/ssmtp/ssmtp.conf
-sleep 5
 service postfix restart
-sleep 5
 echo " adding email crotab"
 crontab cron.sh
 echo " Mail setup is testing"
@@ -74,5 +68,8 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 export kubever=$(kubectl version | base64 | tr -d '\n')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
+echo " Please wait till the kube cluster comes up"
+slepp 60
 kubectl get nodes
+echo "=========================================================="
 echo "ALL are Done"
