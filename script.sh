eee051288cbd2672b83f009fa38d8b7fc849c730
root <root@server1.sub08270515460.vcn22.oraclevcn.com>
2020-09-14 22:20:26 +0530

    ee

--- script.sh
+++ script.sh
@@ -69,7 +69,7 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 export kubever=$(kubectl version | base64 | tr -d '\n')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
 echo " Please wait till the kube cluster comes up"
-slepp 60
+sleep 60
 kubectl get nodes
 echo "=========================================================="
 echo "ALL are Done"
