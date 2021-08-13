4ac500bb311239ab4df8c3e3d500c279887363f1
root <root@server1.us-east1-b.c.gcloud384.internal>
2021-08-13 07:45:29 +0530

    jboss added

---
+++ jboss.sh
@@ -0,0 +1,6 @@
+git clone https://gitlab.com/reddi.devops/jboss-java.git
+cd jboss-java
+tar -xzvf jboss-domain.tar.gz -C /opt/
+cp -rp /opt/jboss-domain /opt/jboss-host
+tar -xzvf domin.tar.gz -C /opt/jboss-host/
+#
